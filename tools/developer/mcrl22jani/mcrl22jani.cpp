391faa5cbef12cd38597c83677a569878c0a4778
wilitp <guilledeipola@gmail.com>
2025-11-27 15:27:11 -0300

    initial setup for tool mcrl22jani

---
+++ tools/developer/mcrl22jani/mcrl22jani.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "mcrl2/lps/linearise.h"
+
+int main() {
+
+
+  /**
+   * OUTLINE
+   *
+   * - Parse mCRL2 spec
+   * - Check that it's linearisable (dispose of the result)
+   * - Use rewriter to remove the use of ecuations and mappings
+   * -  
+    */
+
+  std::cout << "hey!\n";
+}
